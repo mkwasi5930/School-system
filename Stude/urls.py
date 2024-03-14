c57cac93de0ad8d2d48c6dac1a6f840b9dc7e199
Abednego Tati <101582822+mkwasi5930@users.noreply.github.com>
2024-03-14 11:46:33 +0300

    Update urls.py

--- Stude/urls.py
+++ Stude/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('view_own_record/', views.view_own_record, name='view_own_record'),
+    path('edit_personal_details/', views.edit_personal_details, name='edit_personal_details'),
+]
 
