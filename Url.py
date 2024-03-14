c3731f1a81bc717c5434798aa378d36338d0bbaa
Abednego Tati <101582822+mkwasi5930@users.noreply.github.com>
2024-03-14 11:45:01 +0300

    Update Urls.py

--- Url.py
+++ Url.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('admin/', include('admin.urls', namespace='admin')),
+    path('students/', include('students.urls', namespace='students')),
+]
